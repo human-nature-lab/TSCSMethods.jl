4ce62bc85d64cb791a6f2bb56e8bcfb5edcc64f2
Eric Martin Feltham <eric.feltham@yale.edu>
2021-02-17 14:03:25 -0500

    minor change

--- src/tscsmethods.jl
+++ src/tscsmethods.jl
@@ -5,14 +5,15 @@ module tscsmethods
 using StatsBase, Statistics
 
 import LinearAlgebra.Diagonal
-import IterTools.product
+import IterTools.product # this may be deprecated
+
+using CSV, DataFrames, DataFramesMeta
 
 using Gadfly
 import Cairo, Fontconfig
 
-using CSV, DataFrames, DataFramesMeta
-
 # include code files here
+
 include("helper_functions.jl")
 include("matching_functions.jl")
 include("balancing_functions.jl")
@@ -20,8 +21,9 @@ include("estimation_functions.jl")
 include("get_wits.jl")
 include("bootstrap_estimation.jl")
 include("plotting_functions.jl")
-# include("load_and_clean.jl"); doesn't go into pkg
-# need to explain how the data should be formatted, etc.
+# include("load_and_clean.jl");
+  #= doesn't go into pkg
+  need to explain how the data should be formatted, etc. =#
 
 
 end # module tscsmethods
