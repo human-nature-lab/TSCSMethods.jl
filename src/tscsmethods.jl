38ca125b7de088391748d395a3c487dad8aaa486
Eric Martin Feltham <eric.feltham@yale.edu>
2021-02-17 13:58:59 -0500

    skeleton added

--- src/tscsmethods.jl
+++ src/tscsmethods.jl
@@ -1,7 +1,27 @@
 module tscsmethods
 
-# Write your package code here.
+# load all dependencies
 
-# test change
+using StatsBase, Statistics
 
-end
+import LinearAlgebra.Diagonal
+import IterTools.product
+
+using Gadfly
+import Cairo, Fontconfig
+
+using CSV, DataFrames, DataFramesMeta
+
+# include code files here
+include("helper_functions.jl")
+include("matching_functions.jl")
+include("balancing_functions.jl")
+include("estimation_functions.jl")
+include("get_wits.jl")
+include("bootstrap_estimation.jl")
+include("plotting_functions.jl")
+# include("load_and_clean.jl"); doesn't go into pkg
+# need to explain how the data should be formatted, etc.
+
+
+end # module tscsmethods
