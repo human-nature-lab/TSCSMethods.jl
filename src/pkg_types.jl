b26812b6b2b2540cd678b1f7fdc5b015c3361dfa
Eric Martin Feltham <eric.feltham@yale.edu>
2021-03-10 15:09:53 -0500

    add pkg_types.jl
    begin move toward use of model object struct. this will significantly streamline usage of the package

---
+++ src/pkg_types.jl
@@ -0,0 +1,59 @@
+using Parameters
+
+#=
+model object
+* the data is kept separately
+=#
+r_df = Union{Base.RefValue{DataFrame}, DataFrame}; # type for model
+
+@with_kw mutable struct cicmodel
+  title::String = "standard"
+  id::Symbol
+  t::Symbol
+  outcome::Symbol
+  treatment::Symbol
+  matchingcovar::Vector{Symbol}
+  caliper::Dict{Symbol, Float64} = Dict{Symbol, Float64}()
+  fmin::Int64
+  fmax::Int64
+  stratvar::Symbol = Symbol()
+  tpoint::Int64
+  tmin::Int64 # gives length of matching period with tpoint
+  refinementnum::Int64 = Int64(0)
+  
+  # store either dataframe or reference to dataframe, dep on use cases
+  matches::r_df = Base.RefValue{DataFrame}()
+  matches5::r_df = Base.RefValue{DataFrame}()
+
+  data::r_df = Base.RefValue{DataFrame}()
+  
+  # balances pre refinement
+  balances_pre::DataFrame = DataFrame()
+  meanbalances_pre::DataFrame = DataFrame()
+  # post refinement
+  balances_post::DataFrame = DataFrame()
+  meanbalances_post::DataFrame = DataFrame()
+
+  # we may also want to store the full bootstrap estimates
+
+  # one results per model, so store the DF
+  boot_iterations::Int64 = 500
+  results::DataFrame = DataFrame()
+  boot_estimates::Array{Float64} = Array{Float64}(undef)
+
+  # plot slots
+
+  # covariate balance pre and post refinement
+  pl_cb_pre::Plot = Plot()
+  pl_cb_post::Plot = Plot()
+
+  # att estimates pre and post refinement
+  pl_att_pre::Plot = Plot()
+  pl_att_post::Plot = Plot()
+  
+  # total number of treated observations
+  treatednum::Int64 = Int64(0)
+  
+  # number of treated left over after filtering or caliper
+  treatedleft::Int64 = Int64(0) # left after caliper
+end
