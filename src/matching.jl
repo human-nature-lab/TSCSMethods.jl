f38277b49de73bd2ac705bb06e49c152d1bee254
Eric Martin Feltham <eric.feltham@yale.edu>
2021-12-04 16:42:51 -0500

    add back matchassignments

--- src/matching.jl
+++ src/matching.jl
@@ -342,7 +342,17 @@ end
 
 ###
 
-
+function matchassignments(tobsi, ids; returnefsets = true)
+  assigned = getassigned(tobsi.mus, tobsi.fs);
+  emus = ids[assigned]; # eligible matches
+  
+  if returnefsets == false
+    return emus
+  else
+    efsets = tobsi.fs[assigned]; # allowable fs for each eligible match
+    return emus, efsets
+  end
+end
 
 function _distances!(
   tobs, observations, ids,
