855ced1157f060097944b1e8d9376e5f5f37e657
Eric Martin Feltham <eric.feltham@yale.edu>
2022-03-09 18:29:13 -0500

    fix error

--- src/make_groupindices.jl
+++ src/make_groupindices.jl
@@ -6,7 +6,7 @@ STypeMat = SubArray{Float64, 2, Matrix{Float64}, Tuple{Vector{Int64}, Base.Slice
 STypeMatMis = SubArray{Union{Missing, Float64}, 2, Matrix{Union{Missing, Float64}}, Tuple{Vector{Int64}, Base.Slice{Base.OneTo{Int64}}}, false}
 
 STypeVec = SubArray{Int64, 1, Vector{Int64}, Tuple{Vector{Int64}}, false}
-STypeVecBool = SubArray{Bool, 1, Vector{Bool}, Tuple{Vector{eltype(treatvec)}}, false}
+STypeVecBool = SubArray{Bool, 1, Vector{Bool}, Tuple{Vector{Bool}}, false}
 # last entry probably depends on original datatype before conversion
 
 """
