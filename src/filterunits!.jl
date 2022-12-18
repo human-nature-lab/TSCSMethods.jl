730c4a480da52877a2270ec6e536facc3e4e579b
Eric Martin Feltham <eric.feltham@yale.edu>
2022-12-18 15:00:03 -0500

    missing for filterunits!

--- src/filterunits!.jl
+++ src/filterunits!.jl
@@ -10,27 +10,38 @@ function filterunits!(m, omap)
         (tt, tu) = m.observations[i]
 
         _filterunits!(
-            m.matches[i].mus, m.matches[i].ranks, tt, tu, omap, m.F, m.ids
+            m.matches[i].mus, m.matches[i].ranks, tt, tu, omap, m.F, m.ids,
+            m.reference
         )
     end
 end
 
-function _filterunits!(mus, ranks, tt, tu, omap, F, ids)
+function _filterunits!(mus, ranks, tt, tu, omap, F, ids, refval)
+    rf = tt + refval
+    if isnan(get(omap, (rf, tu), NaN)) | ismissing(get(omap, (rf, tu), missing))
+        @views mus[:, :] .= false
+        for ky in eachindex(ranks)
+            ranks[ky] = Int[]
+        end
+        return 
+    end
+
     for (j, f) in enumerate(F)
+
         ot = f + tt # outcome
         musj = @views mus[:, j]
-        if isnan(get(omap, (ot, tu), NaN))
+        if isnan(get(omap, (ot, tu), NaN)) | ismissing(get(omap, (ot, tu), missing))
             musj .= false # whole column is trashed
             ranks[j] = Int[] # no ranks for trt obs at f
         else
-            _filtermu!(musj, ranks[j], ot, ids, omap)
+            _filtermu!(musj, ranks[j], ot, refval, ids, omap)
         end
     end
 end
 
-function _filtermu!(musj, ranksj, ot, ids, omap)
+function _filtermu!(musj, ranksj, ot, refval, ids, omap)
     for (l, mu) in enumerate(ids)
-        if isnan(get(omap, (ot, mu), NaN))
+        if isnan(get(omap, (ot, mu), NaN)) | ismissing(get(omap, (ot, mu), missing)) | isnan(get(omap, (refval, mu), NaN)) | ismissing(get(omap, (refval, mu), missing))
             musj[l] = false
             # ranks is in best-match order
             # find match to remove from ranks
