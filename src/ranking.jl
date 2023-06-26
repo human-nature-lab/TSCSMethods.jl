cf640a4cccfa597dcd12b09be700f68aea2d6234
Eric Martin Feltham <eric.feltham@yale.edu>
2023-06-26 17:28:29 -0400

    remove duplicate

--- src/ranking.jl
+++ src/ranking.jl
@@ -73,32 +73,3 @@ function _fixed_rankmatches!(ranks, fulldist, distances, mus, flen, valids)
   end
   return ranks
 end
-
-function _rankmatches!(ranks, fbools, mamatc)
-
-  # (φ, (fbool, ec, mc)) = collect(enumerate(zip(fbools, mamatc, musc)))[1]
-
-  for (φ, (fbool, ec)) in enumerate(zip(fbools, mamatc))
-    if fbool
-      # in-place ranking
-      # ranks[φ] = StatsBase.ordinalrank(ec);
-      # ranks[φ][isinf.(ec)] .= 0; # make zero if not valid
-
-      # really, we want to be able to shut off the mus easily
-      # and make it such that we get the mu elements in ranked order
-      # by simple indexing:
-      # ranks[φ] = @views mc[sortperm(ec)];
-  
-      # give indices based on ids
-      # only give valid ones
-      ranks[φ] = sortperm(ec)[1:findfirst(isinf.(ec[sortperm(ec)]))-1]
-
-      # remove impossible mus (for that f)
-      # ranks[φ][findfirst(isinf.(ec[sortperm(ec)])):end] .= 0
-        # Inf values should never be used, in any model
-      # example refinement application:
-      # ormus[1+5:end] .= 0
-    end
-  end
-  return ranks
-end
